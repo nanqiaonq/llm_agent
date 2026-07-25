1f5ab7bc08fff03de20faac6440c3e36ba3dff7a
nanqiao <nqdddyx@163.com>
2026-07-25 14:30:54 +0800

    dd

--- step2_agent开发实战上手/part6_Agent_skills/part2_agent_skills_设计实战/mini-OpenClaw项目源码/frontend/src/app/providers.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-
-import { AppProvider } from "@/lib/store";
-
-export default function Providers({ children }: { children: React.ReactNode }) {
-  return <AppProvider>{children}</AppProvider>;
-}
